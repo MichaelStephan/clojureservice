8c95423202d9c61b50d18c563b599f69eea7d65c
Michael Stephan <michael.stephan@sap.com>
2016-08-02 09:11:28 +0200

    update

--- project.clj
+++ project.clj
@@ -1,18 +1,13 @@
 (defproject service "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A simple price"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [http-kit "2.1.18"]
-                 [compojure "1.3.4"]
-                 [environ "1.0.0"]
-                 [com.taoensso/timbre "3.4.0"]
-                 [cljfmt "0.1.10"]
-                 [jonase/eastwood "0.2.1"]
-                 [javax.servlet/servlet-api "2.5"]]
-  :main service.core
-  :plugins [[jonase/eastwood "0.2.1"]]
-  :profiles {:dev {}
-             :production {}})
+                 [compojure "1.5.1"]
+                 [environ "1.0.3"]
+                 [com.taoensso/timbre "4.7.3"]
+                 [javax.servlet/servlet-api "2.5"]
+                 [org.clojure/core.async "0.2.385"]]
+  :main service.core)
--- src/service/core.clj
+++ src/service/core.clj
@@ -1,14 +1,14 @@
 (ns service.core
-
   (:gen-class)
   (:require
-   [taoensso.timbre :as log] 
-   [environ.core :refer [env]]
-   [org.httpkit.server :as srv])
+    [taoensso.timbre :as log] 
+    [environ.core :refer [env]]
+    [org.httpkit.server :as srv :refer [send! with-channel]]
+    [clojure.core.async :as a :refer [go <! >! timeout]])
   (:use
-   [compojure.route :only [files not-found]]
-   [compojure.handler :only [site]]
-   [compojure.core :only [defroutes GET POST DELETE ANY context]]))
+    [compojure.route :only [files not-found]]
+    [compojure.handler :only [site]]
+    [compojure.core :only [defroutes GET POST DELETE ANY context]]))
 
 (def default-port 9000)
 
@@ -20,23 +20,31 @@
 
 (defonce server (atom nil))
 
-(defn stop-server []
-  (when-not (nil? @server)
-    (@server :timeout 100)
-    (reset! server nil)))
-
-(defn home []
-  {:status 200
-   :headers {"Content-Type" "application/json"}
-   :body "{}"})
+(defn home [req]
+  (with-channel req channel
+    (go
+      (send! channel 
+        {:status 200
+         :headers {"Content-Type" "application/json"}
+         :body "{}"}))))
 
 (defroutes all-routes
-  (GET "/" [] (home))
+  (GET "/" req (home req))
   (not-found {:status 404 
               :headers {"Content-Type" "text/html"}
               :body    "resource not found"}))
 
+(defn start-server! [port]
+  (log/info "server listening on port " port)
+  (reset! server (srv/run-server #'all-routes {:port port}))
+  true)
+
+(defn stop-server! []
+  (when-not (nil? @server)
+    (@server :timeout 100)
+    (reset! server nil)
+    true))
+
 (defn -main []
-  (let [port (port)]
-    (log/info "server listening on port " port)
-    (reset! server (srv/run-server #'all-routes {:port port}))))
+  (stop-server!)
+  (start-server! (port)))
